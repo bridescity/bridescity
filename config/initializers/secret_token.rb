fe6af9417f399645b0e139ad8158dc740792f901
everm1nd <everm1nd.mail@gmail.com>
2011-03-06 03:26:18 +0300

    alpha

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bridescity::Application.config.secret_token = 'bdf89ede93b277ddb97212c77f1b19b865671260117d59277cebe90b36900b681a4596dc57d680cfd00525216eef28143d3646befb0f703559e9a197bc978267'
