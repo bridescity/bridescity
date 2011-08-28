17311d989eed71e14824f0078a063a7ec3931302
Pavel Ivanov <ardentum@gmail.com>
2011-08-28 18:20:55 +0400

    Сверстана страница редактирования личной инфы.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
 	def button value, options = {:class => ""}
 		content_tag :button, value, options
 	end
+
+	def edit_current_user_path
+		url_for(current_user) + "/edit"
+	end
 end
